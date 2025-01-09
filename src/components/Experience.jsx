325719d0f6634d036311f58d8044feafda1ba366
BarsatKhadka <khadkabarsat598@gmail.com>
2025-01-09 09:37:29 -0600

    add: some margins to make mobile-friendly

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -9,10 +9,10 @@ export const Experience = () =>{
            <p><span className="text-sm text-gray-400 ml-6">December 2022 - March 2023</span> 
            <ul className="list-disc list-inside mt-2 text-sm text-gray-300 ml-6">
             <li className="mb-2 sm:mr-2">Tutored 30+ grade 11 Students in mastering core Java concepts including loops , OOP , Collections Framework , JDBC , and Generics.  </li>
-            <li className="mb-2">Explained fundamental data structures (e.g., arrays, linked lists, stacks, queues) and their trade-offs to build a strong programming foundation.</li>
-            <li className="mb-2">Introduced students to development tools like IntelliJ IDEA and Eclipse for writing and debugging Java code.</li>
-            <li className="mb-2">Guided students in using version control systems like Git for collaborative coding and project management.</li>
-            <li> Assigned and supervised small-scale projects (e.g., library management systems, student grade calculators) to apply Java concepts in real-world scenarios.</li>
+            <li className="mb-2 sm: mr-2">Explained fundamental data structures (e.g., arrays, linked lists, stacks, queues) and their trade-offs to build a strong programming foundation.</li>
+            <li className="mb-2 sm:mr-2">Introduced students to development tools like IntelliJ IDEA and Eclipse for writing and debugging Java code.</li>
+            <li className="mb-2 sm:mr-2">Guided students in using version control systems like Git for collaborative coding and project management.</li>
+            <li className="mr-2"> Assigned and supervised small-scale projects (e.g., library management systems, student grade calculators) to apply Java concepts in real-world scenarios.</li>
             </ul>
            </p>
            
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,17 +5,17 @@ export const Header = () =>{
      <p className="mb-4 text-[#ebffff] "> Barsat Khadka</p>
 
      <p className="text-sm mb-2 sm:mr-8 ">
-      <span className="text-[#eebc86]">Languages:</span>
+      <span className="text-[#eebc86] sm: mr-2">Languages:</span>
       &nbsp;&nbsp;&nbsp;&nbsp;
       Java , Python , Javascript , Typescript , SQL ,  C++ 
       </p>
 
      <p className="text-sm mb-2 sm:mr-8">
-      <span className="text-[#eebc86]">Frameworks:</span>&nbsp; &nbsp;SpringBoot , Spring Security , React.js , Tailwind CSS  
+      <span className="text-[#eebc86] ">Frameworks:</span>&nbsp; &nbsp;SpringBoot , Spring Security , React.js , Tailwind CSS  
       </p>
 
      <p className="text-sm sm:mr-8">
-     <span className="text-[#eebc86]"> Extras: </span>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; JPA , Maven , Postman API , Docker , Git , AWS , Linux
+     <span className="text-[#eebc86] sm: mr-2"> Extras: </span>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; JPA , Maven , Postman API , Docker , Git , AWS , Linux
       </p>
 
     </div>
