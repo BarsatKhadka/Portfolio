b9daba499fd435e6df92d5fe4d29349549f5edc7
BarsatKhadka <khadkabarsat598@gmail.com>
2025-01-09 13:46:38 -0600

    add: Highlighting words in first project card

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -19,7 +19,7 @@ export const Projects = () => {
                 <span className="cursor-text ml-4">Easy Repo</span>
             </div>
             <div className="flex gap-2">
-                <span className="bg-[#000000]/10 text-white text-xs px-3 py-1 rounded-sm border border-[#6DB33F] flex items-center gap-1">
+                <span className="bg-[#000000]/10 text-white text-xs px-3 py-1 rounded-sm border border-[#95B5] flex items-center gap-1">
                 <FaGithub className="inline text-white" /> Source
                 </span>
                 <span className="relative bg-gradient-to-r from-[#6DB33F] to-[#8BC34A] text-black text-xs px-4 py-2 rounded-lg border border-[#6DB33F]/50 hover:shadow-lg hover:shadow-[#6DB33F]/40 transition-all duration-300 hover:scale-105 group">
@@ -31,8 +31,15 @@ export const Projects = () => {
             <p className="text-gray-400 text-sm mb-4 cursor-text">
               GitHub Repository Management Tool
             </p>
-            <p className="text-gray-300 text-sm mb-4 cursor-text">
-              A full-stack web application to simplify GitHub repository management. Features include secure OAuth2 login, repository grouping, renaming and deleting a repository, directly opening in VS code, viewing repos in tree structure and lines of code, and ultimately can be controlled by a CLI present in the website.
+            <p className="text-gray-300 text-sm mb-4 cursor-text" >
+            Easy Repo is a powerful full-stack web application designed to simplify your GitHub repository management experience. With secure
+             <span className="font-bold text-[#95B54C]"> OAuth2 login </span>, you can effortlessly 
+            <span className="font-bold text-[#E58C8A]"> organize your repositories into groups, rename or delete them  </span>, and even  
+            <span className="font-bold text-[#95B54C]"> open them directly in VS Code</span> 
+            . Explore your repositories in a 
+            <span className="font-bold text-[#E58C8A]"> detailed tree structure, analyze lines of code, </span> 
+            and manage everything seamlessly through an
+            <span className="font-bold text-[#95B54C]"> integrated CLI </span>available on the website
             </p>
             <div className="flex flex-wrap gap-2">
               <span className="bg-[#6DB33F]/10 text-[#6DB33F] text-xs px-3 py-1 rounded-full cursor-text">SpringBoot</span>
@@ -49,7 +56,7 @@ export const Projects = () => {
                 <span className="cursor-text ml-4">AI Dating Bot</span>
             </div>
               <div className="flex gap-2">
-              <span className="bg-[#000000]/10 text-white text-xs px-3 py-1 rounded-sm border border-[#6DB33F] flex items-center gap-1">
+              <span className="bg-[#000000]/10 text-white text-xs px-3 py-1 rounded-sm border border-[#95B5] flex items-center gap-1">
                   <FaGithub className="inline text-white" /> Source
                 </span>
                 <span className="relative bg-gradient-to-r from-[#6DB33F] to-[#8BC34A] text-black text-xs px-4 py-2 rounded-lg border border-[#6DB33F]/50 hover:shadow-lg hover:shadow-[#6DB33F]/40 transition-all duration-300 hover:scale-105 group">
@@ -79,7 +86,7 @@ export const Projects = () => {
                 <span className="cursor-text ml-4">Safe Store DBMS</span>
             </div>
               <div className="flex gap-2">
-              <span className="bg-[#000000]/10 text-white text-xs px-3 py-1 rounded-sm border border-[#6DB33F] flex items-center gap-1">
+              <span className="bg-[#000000]/10 text-white text-xs px-3 py-1 rounded-sm border border-[#95B5] flex items-center gap-1">
                   <FaGithub className="inline text-white" /> Source
                 </span>
                 <span className="relative bg-gradient-to-r from-[#6DB33F] to-[#8BC34A] text-black text-xs px-4 py-2 rounded-lg border border-[#6DB33F]/50 hover:shadow-lg hover:shadow-[#6DB33F]/40 transition-all duration-300 hover:scale-105 group">
