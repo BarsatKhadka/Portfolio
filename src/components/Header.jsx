cc02362161afe9b74f24176984f6564f07e8e415
BarsatKhadka <khadkabarsat598@gmail.com>
2025-01-08 21:37:24 -0600

    add: Margin for smaller devices in header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,9 @@ export const Header = () =>{
     <>
     <div className="ml-8 mt-8 lg:ml-48 ">
      <p className="mb-8"> Barsat Khadka</p>
-     <p className="text-sm mb-2">Languages:&nbsp;&nbsp;&nbsp;&nbsp; Java , Python , Javascript , Typescript , SQL ,  C++</p>
-     <p className="text-sm mb-2">Frameworks:&nbsp; &nbsp;SpringBoot , Spring Security , React.js , Tailwind CSS  </p>
-     <p className="text-sm">Extras: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; JPA , Maven , Postman API , Docker , Git , AWS , Linux</p>
+     <p className="text-sm mb-2 sm:mr-8">Languages:&nbsp;&nbsp;&nbsp;&nbsp; Java , Python , Javascript , Typescript , SQL ,  C++</p>
+     <p className="text-sm mb-2 sm:mr-8">Frameworks:&nbsp; &nbsp;SpringBoot , Spring Security , React.js , Tailwind CSS  </p>
+     <p className="text-sm sm:mr-8">Extras: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; JPA , Maven , Postman API , Docker , Git , AWS , Linux</p>
     </div>
     </>
   )
