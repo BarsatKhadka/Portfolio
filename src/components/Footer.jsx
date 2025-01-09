a55908783b1e235ff920edb4389c71014cc12e8f
BarsatKhadka <khadkabarsat598@gmail.com>
2025-01-09 16:03:05 -0600

    add: Description for projects in project section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,13 @@
 import { FaGithub, FaLinkedin, FaTwitter } from "react-icons/fa";
 import { SiLeetcode } from "react-icons/si";
 
+
 export const Footer = () => {
   return (
     <footer className="bg-[#1C1917] border-t border-[#000000]/10 py-4 fixed  bottom-0 left-0 w-full sm:mt-4">
       <div className="container mx-auto px-4 lg:px-48">
         <div className="flex items-center justify-between">
-          <p className="text-[#eebc86] text-sm">
+          <p className="text-[#] text-sm">
             &copy; {new Date().getFullYear()} Barsat Khadka.
           </p>
 
@@ -15,7 +16,7 @@ export const Footer = () => {
               href="https://github.com/barsatKhadka"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-[#eebc86] hover:text-[#eebc86]/80 transition-all duration-300 hover:scale-110"
+              className="text-[#] hover:text-[#eebc86]/80 transition-all duration-300 hover:scale-110"
             >
               <FaGithub className="w-5 h-5" />
             </a>
@@ -23,7 +24,7 @@ export const Footer = () => {
               href="https://www.linkedin.com/in/barsat-khadka/"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-[#eebc86] hover:text-[#eebc86]/80 transition-all duration-300 hover:scale-110"
+              className="text-[#] hover:text-[#eebc86]/80 transition-all duration-300 hover:scale-110"
             >
               <FaLinkedin className="w-5 h-5"/>
             </a>
@@ -31,7 +32,7 @@ export const Footer = () => {
               href="https://leetcode.com/u/BarsatKhadka/"
               target="_blank"
               rel="noopener noreferrer"
-              className="text-[#eebc86] hover:text-[#eebc86]/80 transition-all duration-300 hover:scale-110"
+              className="text-[] hover:text-[#eebc86]/80 transition-all duration-300 hover:scale-110"
             >
               <SiLeetcode className="w-5 h-5" />
             </a>
