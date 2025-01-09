52512a3024efbf3fe7bfaca3168939031ae1ef37
BarsatKhadka <khadkabarsat598@gmail.com>
2025-01-08 22:34:51 -0600

    add: Font family and some colors

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,22 @@ export const Header = () =>{
   return(
     <>
     <div className="ml-8 mt-8 lg:ml-48 ">
-     <p className="mb-8"> Barsat Khadka</p>
-     <p className="text-sm mb-2 sm:mr-8">Languages:&nbsp;&nbsp;&nbsp;&nbsp; Java , Python , Javascript , Typescript , SQL ,  C++</p>
-     <p className="text-sm mb-2 sm:mr-8">Frameworks:&nbsp; &nbsp;SpringBoot , Spring Security , React.js , Tailwind CSS  </p>
-     <p className="text-sm sm:mr-8">Extras: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; JPA , Maven , Postman API , Docker , Git , AWS , Linux</p>
+     <p className="mb-8 "> Barsat Khadka</p>
+
+     <p className="text-sm mb-2 sm:mr-8 ">
+      <span className="text-[#eebc86]">Languages:</span>
+      &nbsp;&nbsp;&nbsp;&nbsp;
+      Java , Python , Javascript , Typescript , SQL ,  C++ 
+      </p>
+
+     <p className="text-sm mb-2 sm:mr-8">
+      <span className="text-[#eebc86]">Frameworks:</span>&nbsp; &nbsp;SpringBoot , Spring Security , React.js , Tailwind CSS  
+      </p>
+
+     <p className="text-sm sm:mr-8">
+     <span className="text-[#eebc86]"> Extras: </span>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; JPA , Maven , Postman API , Docker , Git , AWS , Linux
+      </p>
+
     </div>
     </>
   )
