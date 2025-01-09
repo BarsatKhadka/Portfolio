74e79f17d5d7b10e9ff32de78ea6c167b703ed16
BarsatKhadka <khadkabarsat598@gmail.com>
2025-01-08 21:29:30 -0600

    add: Header Section(skill section)

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
+import { Header } from "./components/Header"
 function App() {
   return (
     <>
-      <h1 className='underline'>Hello World</h1>
+      <Header/>
+    
     </>
   )
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+export const Header = () =>{
+  return(
+    <>
+    <div className="ml-8 mt-8 lg:ml-48 ">
+     <p className="mb-8"> Barsat Khadka</p>
+     <p className="text-sm mb-2">Languages:&nbsp;&nbsp;&nbsp;&nbsp; Java , Python , Javascript , Typescript , SQL ,  C++</p>
+     <p className="text-sm mb-2">Frameworks:&nbsp; &nbsp;SpringBoot , Spring Security , React.js , Tailwind CSS  </p>
+     <p className="text-sm">Extras: &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; JPA , Maven , Postman API , Docker , Git , AWS , Linux</p>
+    </div>
+    </>
+  )
+  
+}
