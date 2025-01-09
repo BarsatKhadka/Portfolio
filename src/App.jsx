d18e6713dfd345613a37d2c38230158ab3c6c28c
BarsatKhadka <khadkabarsat598@gmail.com>
2025-01-09 09:33:10 -0600

    add: Projects Sections UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header"
 import { Experience} from "./components/Experience"
+import { Projects } from "./components/Projects"
 function App() {
   return (
     <>
       <Header/>
       <Experience/>
+      <Projects/>
     
     </>
   )
---
+++ src/components/Projects.jsx
@@ -0,0 +1,56 @@
+import { IoFolderOpenOutline } from "react-icons/io5";
+
+export const Projects = () => {
+  return (
+    <>
+      <div className="ml-8 mt-16 lg:ml-48">
+        <p className="text-[#ebffff] mb-6"> 
+          <IoFolderOpenOutline className="inline mr-1" /> Projects 
+        </p>
+        
+        
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mr-8 lg:mr-48">
+
+          
+          <div className="bg-[#] p-6 rounded-lg hover:scale-105 transform transition-all duration-500 hover:shadow-2xl hover:shadow-[#eebc86]/20 cursor-pointer">
+            <h3 className="text-[#ebffff] text-xl font-semibold mb-2">Easy Repo</h3>
+            <p className="text-gray-300 text-sm mb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate sunt harum aliquam quibusdam, rem facere accusamus et incidunt optio nisi suscipit sequi magni repellat vel nulla mollitia. Similique, voluptatibus id!
+            </p>
+            <div className="flex flex-wrap gap-2">
+              <span className="bg-[#6DB33F]/10 text-[#6DB33F] text-xs px-3 py-1 rounded-full">SpringBoot</span>
+              <span className="bg-[#61DAFB]/10 text-[#61DAFB] text-xs px-3 py-1 rounded-full">React + Typescript</span>
+              <span className="bg-[#00758F]/10 text-[#61DAFB] text-xs px-3 py-1 rounded-full">MySQL</span>
+            </div>
+          </div>
+
+          
+          <div className="bg-[#] p-6 rounded-lg hover:scale-105 transform transition-all duration-300 hover:shadow-2xl hover:shadow-[#eebc86]/20 cursor-pointer">
+            <h3 className="text-[#] text-xl font-semibold mb-2">AI Dating bot</h3>
+            <p className="text-gray-300 text-sm mb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus nobis perferendis dicta, accusantium laboriosam obcaecati. Modi et ad, laudantium nam asperiores dolorem quam tenetur adipisci id officiis. Ipsum, magni doloremque.
+            </p>
+            <div className="flex flex-wrap gap-2">
+              <span className="bg-[#00ffff]/10 text-[#00ffff] text-xs px-3 py-1 rounded-full">language</span>
+              <span className="bg-[#00ffff]/10 text-[#00ffff] text-xs px-3 py-1 rounded-full">language</span>
+              <span className="bg-[#00ffff]/10 text-[#00ffff] text-xs px-3 py-1 rounded-full">language</span>
+            </div>
+          </div>
+
+
+          <div className="bg-[#] p-6 rounded-lg hover:scale-105 transform transition-all duration-300 hover:shadow-2xl hover:shadow-[#eebc86]/20 cursor-pointer">
+            <h3 className="text-[#] text-xl font-semibold mb-2">Database Backup Utility</h3>
+            <p className="text-gray-300 text-sm mb-4">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Rerum deleniti velit delectus, quis debitis nihil minus animi quo quisquam culpa deserunt commodi perspiciatis voluptatum nostrum, sapiente labore laudantium, praesentium laborum!
+            </p>
+            <div className="flex flex-wrap gap-2">
+              <span className="bg-[#00ffff]/10 text-[#00ffff] text-xs px-3 py-1 rounded-full">language</span>
+              <span className="bg-[#00ffff]/10 text-[#00ffff] text-xs px-3 py-1 rounded-full">language</span>
+              <span className="bg-[#00ffff]/10 text-[#00ffff] text-xs px-3 py-1 rounded-full">language</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
